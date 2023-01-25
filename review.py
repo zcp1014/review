8d3bef67f78795811afe288c6fed5b63a8cd0515
ching Ping <sorrymyfrance@hotmail.com>
2023-01-25 19:47:44 +0800

    'version2'

--- review.py
+++ review.py
@@ -6,4 +6,11 @@ with open('reviews.txt','r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+
+print('檔案總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('留言的平均長度是',sum_len/len(data))
